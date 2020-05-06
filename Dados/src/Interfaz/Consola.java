9765fa28b1237add7d12f47bc07399f5817ffbd5
Mateo <mateo.sapiurka@gmail.com>
2020-05-05 21:48:28 -0300

    Menu
    Menu en Main y algunos metodo menu() en Consola

--- Dados/src/Interfaz/Consola.java
+++ Dados/src/Interfaz/Consola.java
@@ -5,27 +5,54 @@
  */
 package Interfaz;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Mateo
  */
 public class Consola {
-    
-    public void saludar(){
-        
+
+    public void saludar() {
+
         System.out.println("BIENVENIDO A DADOS!");
-        }
-    
-    public void imprimirMenu(){    
-        
+    }
+
+    public void menu() {
+
         System.out.println("MENÚ:");
         System.out.println("1.REGISTRAR JUGADOR");
         System.out.println("2.JUGAR");
         System.out.println("3.VER RANKING");
         System.out.println("4.SALIR");
-    }
-    
-    public void pedirMenu(){
-    
+
+        Scanner in = new Scanner(System.in);
+        int respuesta;
+
+        respuesta = in.nextInt();
+        switch (respuesta) {
+            case 1:
+                //CODIGO ACA
+                System.out.println("REGISTRAR UN JUGADOR NUEVO!");
+                //registroJugadores();
+                break;
+            case 2:
+                //CODIGO ACA
+                System.out.println("COMENZAR PARTIDA!");
+                //partida();
+                break;
+            case 3:
+                //CODIGO ACA
+                System.out.println("ESTE ES EL RANKING");
+                //mostrarRanking();
+                break;
+            case 4:
+                System.out.println("GRACIAS POR JUGAR, HASTA LA VUELTA ;)");
+                break;
+            default:
+                System.out.println("ERROR, VUELVA A SELECCIONAR");
+                menu();
+
+        }
     }
 }
--- Dados/src/Interfaz/Main.java
+++ Dados/src/Interfaz/Main.java
@@ -1,13 +1,10 @@
-package Logica;
-
-import Interfaz.Consola;
+package Interfaz;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Mateo
@@ -21,8 +18,8 @@ public class Main {
         // TODO code application logic here
         Consola consola = new Consola();
         consola.saludar();
-        consola.imprimirMenu();
-        consola.pedirMenu();
+        consola.menu();
+
     }
-    
+
 }
