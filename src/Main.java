b960052fae37c36af220e12a6269e2875a6cab54
Mateo <mateo.sapiurka@gmail.com>
2020-05-26 18:55:11 -0300

    Main limpiado

--- src/Main.java
+++ src/Main.java
@@ -23,22 +23,12 @@ public class Main {
         // TODO code application logic here
         Sistema s = new Sistema();
         Consola consola = new Consola();
-        Tablero tablero = new Tablero();
+        /*
         s.agregarJugador(new Jugador("seba",21,"sebaalias"));
         s.agregarJugador(new Jugador("mateo",27,"Mateoalias"));
+         */
         consola.saludar();
         consola.menu(s);
-        
-        /*consola.mostrarTablero(tablero.getMatriz());
-
-        int[] posiciones = {9,10,7,5,2,12,14,20,15,1,11,17,6,18,13,19,8,4,3,16};
-        for (int posicion : posiciones) {
-            tablero.ingresarLetra(posicion, "A", "blue");
-        }
-        
-        consola.mostrarTablero(tablero.getMatriz());
-        Consola.printGreen(Integer.toString(tablero.letraEnSecuencia("A", "blue")));*/
-
     }
 
 }
